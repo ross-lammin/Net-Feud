b9a5c9c816c8ab2e103448a0210aae71a973b52d
ross lammin <ross.lammin@first10.co.uk>
2017-10-20 09:49:52 +0100

    Sorted indentation for js.

--- themes/net-feud/assets/js/scripts/burger-menu.js
+++ themes/net-feud/assets/js/scripts/burger-menu.js
@@ -3,26 +3,26 @@
 var burgerMenu = document.querySelector('.header__burger-menu--container');
 var navBar = document.querySelector('.menu-primary-container');
 
-  document.addEventListener('DOMContentLoaded', function () {
-    burgerMenu.addEventListener('click', function () {
-      burgerMenu.classList.toggle('header__burger-menu--change');
-      navBar.classList.toggle('display-block');
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  burgerMenu.addEventListener('click', function () {
+    burgerMenu.classList.toggle('header__burger-menu--change');
+    navBar.classList.toggle('display-block');
+  });
 
 
 
-    var search = document.querySelector('.nav-bar__primary--search');
-    var searchBar = document.querySelector('.header__searchbar--container');
-    var searchBarInput = document.querySelector('.header__searchbar--search-input');
-    var searchBarX = document.querySelector('.header__searchbar--clear');
+  var search = document.querySelector('.nav-bar__primary--search');
+  var searchBar = document.querySelector('.header__searchbar--container');
+  var searchBarInput = document.querySelector('.header__searchbar--search-input');
+  var searchBarX = document.querySelector('.header__searchbar--clear');
 
-    search.addEventListener('click', function () {
-      searchBar.classList.toggle('display-block2');
-      search.classList.toggle('nav-bar__primary--search-color');
-    });
+  search.addEventListener('click', function () {
+    searchBar.classList.toggle('display-block2');
+    search.classList.toggle('nav-bar__primary--search-color');
+  });
 
-    searchBarX.addEventListener('click', function () {
-      searchBarInput.value = '';
-    });
+  searchBarX.addEventListener('click', function () {
+    searchBarInput.value = '';
+  });
 
 });
